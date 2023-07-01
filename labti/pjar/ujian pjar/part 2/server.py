becb651ee2450c32a4dfe4957b870fec1f4f29fb
Sinatra Kusuma <55923779+svecx@users.noreply.github.com>
2023-07-01 10:08:11 +0700

    upload file

---
+++ labti/pjar/ujian pjar/part 2/server.py
@@ -0,0 +1,21 @@
+import socket
+DNS_TABLE = {
+    'www.google.com': '192.168.1.1',
+    'www.facebook.com': '192.168.1.2',
+    'www.yahoo.com': '192.168.1.3',
+    'www.twitter.com': '192.168.1.4',
+    'www.youtube.com': '192.168.1.5',
+    'www.apple.com': '192.168.1.6'
+
+}
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(('localhost', 53))
+print('DNS server is listening...')
+while True:
+    data, address = server_socket.recvfrom(1024)
+    domain_name = data.decode()
+    if domain_name in DNS_TABLE:
+        ip_address = DNS_TABLE[domain_name]
+    else:
+        ip_address = 'Not Found'
+    server_socket.sendto(ip_address.encode(), address)
